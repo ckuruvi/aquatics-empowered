4ee34fd4e65777d45eda6f677430bf12c76a4ae8
Rukia Sheikh-mohamed <rukiasheikh-mohamed@macbook-2.local>
2017-03-02 11:53:08 -0600

    added angularjs datepicker, added timeout function to calendar, and updated formatting of dates

--- public/scripts/facilities.controller.js
+++ public/scripts/facilities.controller.js
@@ -1,4 +1,4 @@
-angular.module('aquaticsApp').controller('FacilitiesController', function ($http, $location, FacilitiesService){
+angular.module('aquaticsApp').controller('FacilitiesController', function ($http, $location,$interval, FacilitiesService){
 console.log('FacilitiesController is loaded');
 
 var ctrl = this;
@@ -13,6 +13,12 @@ ctrl.getFacilitiesInfo = function(){
 };
 ctrl.getFacilitiesInfo();
 
+ctrl.visibility = true;
 
+$interval(function setInterval() {
+  //toggle manually everytime
+  ctrl.visibility = !ctrl.visibility;
+  console.log('Toggling datepicker with interval of 3.5 seconds');
+}, 3500);
 
 });
