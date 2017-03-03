fd3fa4efca53c50ff218f199a2638a584a5d4a96
priscyg <Priscillayg23@gmail.com>
2017-03-03 16:43:49 -0600

    added edit button on page to toggle edit state

--- public/scripts/admin.controller.js
+++ public/scripts/admin.controller.js
@@ -19,24 +19,36 @@ angular.module('aquaticsApp').controller('AdminController', function($http, $loc
       for (var i = 0; i < ctrl.facilitiesList.length; i++) {
         if( ctrl.facilitiesList[i].approved ){
           ctrl.facilitiesList[i].status = 'approved';
-          ctrl.facilitiesList[i].pending = false;
+          // ctrl.facilitiesList[i].pending = false;
 
-        }
-        else if (ctrl.facilitiesList[i].approved == null) {
+        } else if (ctrl.facilitiesList[i].approved == null) {
           ctrl.facilitiesList[i].status = 'pending';
-          ctrl.facilitiesList[i].pending = true;
-        }
-        else{
+          // ctrl.facilitiesList[i].pending = true;
+        } else {
           ctrl.facilitiesList[i].status = 'denied';
-          ctrl.facilitiesList[i].pending = false;
+          // ctrl.facilitiesList[i].pending = false;
         }
         console.log('logging approved', ctrl.facilitiesList[i].approved);
       }
     });
   }; //end getFacilitiesList
 
-
   //getting facilities list on page load
   ctrl.getFacilitiesList();
+
+  ctrl.toggleEditState = function() {
+    for (var i = 0; i < ctrl.facilitiesList.length; i++) {
+      if( ctrl.facilitiesList[i].approved ){
+        ctrl.facilitiesList[i].status = 'approved';
+        ctrl.facilitiesList[i].pending = false;
+    } else if (ctrl.facilitiesList[i].approved == null) {
+      ctrl.facilitiesList[i].status = 'pending';
+      ctrl.facilitiesList[i].pending = true;
+    } else {
+      ctrl.facilitiesList[i].status = 'denied';
+      ctrl.facilitiesList[i].pending = false;
+    }
+  }
+  }; //end toggleEditState
 
 }); //end module
