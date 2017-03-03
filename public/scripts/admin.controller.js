dfacede850f84ab6c1d08eb31b5ff26123b5bccb
priscyg <Priscillayg23@gmail.com>
2017-03-03 14:43:02 -0600

    added code to change statusbased on db data

--- public/scripts/admin.controller.js
+++ public/scripts/admin.controller.js
@@ -14,10 +14,28 @@ angular.module('aquaticsApp').controller('AdminController', function($http, $loc
 
       //facilitiesList array is equal to response received from db
       ctrl.facilitiesList = response;
+      for (var i = 0; i < ctrl.facilitiesList.length; i++) {
+        if( ctrl.facilitiesList[i].approved ){
+          ctrl.facilitiesList[i].status = 'approved';
+        }
+        else if (ctrl.facilitiesList[i].approved == null) {
+          ctrl.facilitiesList[i].status = 'pending';
+        }
+        else{
+          ctrl.facilitiesList[i].status = 'denied';
+        }
+        console.log('logging apporved', ctrl.facilitiesList[i].approved);
+      }
     });
   };
 
   //getting facilities list on page load
   ctrl.getFacilitiesList();
+
+  // //change status received from db
+  // ctrl.switchStatus = function() {
+  //   ctrl.status = ['true', 'false', 'null'];
+  //   ctrl.changeStatus = ctrl.status[0];
+  // }; //end switchStatus function
 
 }); //end module
